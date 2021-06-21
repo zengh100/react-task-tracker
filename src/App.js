c205a31cff384918e6e0d94e3e75b5c548852ecb
hrzenger <zengh100@gmail.com>
2021-06-21 16:09:08 -0700

    try javascript in function App()

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Hairong'
+  const x = false
   return (
-    <div className="App">
+    <div className="container">
       <h1>Hello From React</h1>
+      <h2>Hello {name}</h2>
+      <h2>Hello {x?'Yes':'No'}</h2>
     </div>
   );
 }
