ff1f90820b8527ca3e383131391fa7148e7f750b
hrzenger <zengh100@gmail.com>
2021-06-22 01:33:55 -0700

    add style

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types'
 const Header = ({title}) => {
     return (
         <header>
-            <h1>{title}</h1>
+            <h1 style={{color:'red'}}>{title}</h1>
         </header>
     )
 }
-// Header.defaultProps = {
-//     title: 'task1 tracker',
-// }
+Header.defaultProps = {
+    title: 'task tracker',
+}
 Header.propType = {
     title: PropTypes.string.isRequired
 }
