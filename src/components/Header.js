431477cb4a95615cfee9b0f6e2442c348919d914
hrzenger <zengh100@gmail.com>
2021-06-22 22:49:14 -0700

    add destructed props to pass parameters back to Button.js to make this Button componenet reusable.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,9 @@ const Header = ({title}) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button />
+            <Button color='green' text='Hello'/>
+            <Button color='red' text='Hello 2'/>
+            <Button color='orange' text='Hello 3'/>
         </header>
     )
 }
