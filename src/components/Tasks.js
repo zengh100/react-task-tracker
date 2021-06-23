ca4a8209cf0f82001021e0d312eded0879e0b3db
hrzenger <zengh100@gmail.com>
2021-06-23 00:40:42 -0700

    Add component Task so that it can be reused by component Tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,7 +1,10 @@
+import Task from './Task'
 const Tasks = ({tasks}) => {
     return (
         <>
-          {tasks.map((task)=>(<h3>{task.text}</h3>))}  
+          {tasks.map((task)=>(
+            <Task key={task.id} task={task} />
+          ))}  
         </>
     )
 }
