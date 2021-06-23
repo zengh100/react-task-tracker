388589e4b5d6af2d824e4f3df24d9559781911f1
hrzenger <zengh100@gmail.com>
2021-06-23 00:08:52 -0700

    Move tasks defintion from component Tasks to App.js and pass the tasks to component using props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 //import logo from './logo.svg';
 //import './App.css';
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+  ])  
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,4 @@
-import { useState } from 'react'
-
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting at school',
-            day: 'Feb 6th at 1:30pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Food Shopping',
-            day: 'Feb 5th at 2:30pm',
-            reminder: false,
-        }
-    
-    ])
+const Tasks = ({tasks}) => {
     return (
         <>
           {tasks.map((task)=>(<h3>{task.text}</h3>))}  
