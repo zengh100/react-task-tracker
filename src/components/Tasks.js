6e8482ae021496ddbbf3a47a2adfd62cfac9021b
hrzenger <zengh100@gmail.com>
2021-06-22 23:59:42 -0700

    import useState from react so that tasks is defined within the Task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,27 @@
-//import React from 'react'
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    }
+import { useState } from 'react'
 
-]
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeting at school',
+            day: 'Feb 6th at 1:30pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Food Shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: false,
+        }
+    
+    ])
     return (
         <>
           {tasks.map((task)=>(<h3>{task.text}</h3>))}  
